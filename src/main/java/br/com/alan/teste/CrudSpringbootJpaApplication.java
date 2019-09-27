1c988e148a2c8f8d71afe797fcc4f66a26c8a3a1
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-09-27 09:05:53 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/alan/teste/CrudSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alan.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootJpaApplication.class, args);
+	}
+
+}
